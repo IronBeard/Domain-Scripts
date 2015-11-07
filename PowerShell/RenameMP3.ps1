fdbfe41e9f6a5e38323ec97aff61a79b88c5c0cf
IronBeard <GarethPhilpott@hotmail.com>
2015-11-07 22:19:43 +1300

    PowerShell RenameMP3 Script
    This script is designed to take a directory containing MP3 files and
    rename them to my preferred standard using the information in the MP3
    Tag.
    
    It came about as I currently have over 18000 MP3 files which are not all
    well named or at least not named to my standard.
    
    Still in development.

---
+++ PowerShell/RenameMP3.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.SYNOPSIS
+    .
+.DESCRIPTION
+    Renames MP3 file using the attributes.
+.NOTES
+    File Name      : RenameMP3.ps1
+    Author         : Gareth Philpott
+    Date           : 06/11/2015
+    Prerequisite   : PowerShell V2 over Vista and upper.
+    Copyright 2015 - Gareth Philpott
+.EXAMPLE
+    RenameMP3
+.LINK
+ 
+#>
+
+#Import-Module Y:\Utilities\MPTag
+
+$media = Get-MediaInfo "W:\Bad Boys\01 - Bad Boys - Diana King - Shy Guy.mp3";
+
+Write-Output $media.Tag.Track;
+Write-Output $media.Tag.Artists;
+Write-Output $media.Tag.Album;
+Write-Output $media.Tag.Title;
+
+$NewAlbum = $media.Tag.Album
+$Artists = $media.Tag.Artists
+$DiscNumber = $media.Tag.Disc
+$Title = $media.Tag.Title;
+$TrackNumber = $media.Tag.Track;
+
+if ($DiscNumber.Length -eq 1) {$DiscNumber="0"+$DiscNumber}
+if ($TrackNumber.Length -eq 1) {$TrackNumber="0"+$TrackNumber}
+
+
+Write-Output "Track $TrackNumber";
+Write-Output "$TrackNumber - $NewAlbum - $Artists - $Title";
