e04702bf825e5597f1b37b9793345e9f5581a2c6
IronBeard <GarethPhilpott@hotmail.com>
2015-11-07 22:32:16 +1300

    PowerShell RemoveAll Script
    This script is designed to take an array of directories/files and
    permanently deletes them.
    
    It came about as my custom backup scripts purposely leave the old files
    and directories in place until they are deemed safe to remove. This is,
    unfortunately, a manual process of finding the files and deleting them
    or "quickly" throwing together a bunch of del commands and running them
    in sequence to get rid of them. This script expands on the del commands
    idea and makes it more extensible.
    
    Still in development.

---
+++ PowerShell/RemoveAll.ps1
@@ -0,0 +1,29 @@
+﻿<#
+.SYNOPSIS
+    Deletes the items from the specified list.
+.DESCRIPTION
+    Iterates throught the supplied list and deletes all matching items.
+.NOTES
+    File Name      : RemoveAll.ps1
+    Author         : Gareth Philpott
+    Date           : 26/10/2015
+    Prerequisite   : PowerShell V2 over Vista and upper.
+    Copyright 2015 - Gareth Philpott
+.EXAMPLE
+    RemoveAll
+.LINK
+ 
+#>
+
+# Grab the items to be deleted from the domain compare scripts
+# ctrl + H and replace the start of the line up to the \\
+# ^[^\\]*
+# ctrl + H and replace \r\n with
+# ",\r\n"
+
+# Need to add a parameter so that the directory list can be sent as a parameter instead of hard coded.
+$myArray =  "\\Hera\Apps\PortableApps\AntRenamerPortable\App\AntRenamer\Languages\German.lng.old",
+            "\\Hera\Apps\PortableApps\AntRenamerPortable\App\AppInfo\Launcher\splash.jpg"
+
+Remove-Item $myArray –whatif
+# Remove-Item $myArray #-recurse
