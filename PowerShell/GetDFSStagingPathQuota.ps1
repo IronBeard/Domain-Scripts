93be5d17b1a681747e1d4596c09b8f2b676bc323
Ironbeard <GarethPhilpott@hotmail.com>
2017-03-05 11:37:47 +1300

    DFS Replication Analysis Script
    This script gets the DFS replication details for all DFS objects so that
    they can be analysed.

---
+++ PowerShell/GetDFSStagingPathQuota.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+    Gets the DFS Staging Path Quota information fromt the Domain.
+.DESCRIPTION
+    Gets the DFS Staging Path Quota for all DFS servers in the Domain and shows the values in a table.
+
+    From https://technet.microsoft.com/library/dn465158.aspx
+.NOTES
+    File Name      : GetDFSStagingPathQuota.ps1
+    Author         : Gareth Philpott
+    Date           : 10/03/2017
+    Prerequisite   : PowerShell V2 over Vista and upper.
+    Copyright 2017 - Gareth Philpott
+.EXAMPLE
+    GetDFSStagingPathQuota
+.LINK
+ 
+#>
+
+Get-DfsrMembership -GroupName * -ComputerName * | Select-Object -Property ComputerName, GroupName, FolderName, ContentPath, StagingPathQuotaInMB, ConflictAndDeletedQuotaInMB | Format-Table
