f0f9137d2e729b22bd1d1abb4052e27ad4b680fa
Ironbeard <IronBeard@users.noreply.github.com>
2021-06-19 21:18:28 +1200

    Update RenameFilesToFolder.ps1
    Added logic to:
    Rename the parent folder if longer than a set value
    Move the files to the root folder
    Delete all of the empty folders

--- PowerShell/RenameFilesToFolder.ps1
+++ PowerShell/RenameFilesToFolder.ps1
@@ -21,7 +21,46 @@
 
 param(
  $Source = "",
- $Filter = ""
+ $Filter = "",
+ $trimLength = 100
 )
+# Original
+#Get-ChildItem $Source -Filter $Filter -Recurse | Rename-Item -NewName {$_.Directory.Name+$_.Extension}
 
-Get-ChildItem $Source -Filter $Filter -Recurse | Rename-Item -NewName {$_.Directory.Name+$_.Extension}
+$tailRecursion = {
+    param(
+        $Path
+    )
+    foreach ($childDirectory in Get-ChildItem -Force -LiteralPath $Path -Directory) {
+        & $tailRecursion -Path $childDirectory.FullName
+    }
+    $currentChildren = Get-ChildItem -Force -LiteralPath $Path
+    $isEmpty = $currentChildren -eq $null
+    if ($isEmpty) {
+        Write-Verbose "Removing empty folder at path '${Path}'." -Verbose
+        Remove-Item -Force -LiteralPath $Path
+    }
+}
+# Testing 
+Get-ChildItem $Source -Filter $filter -Recurse | %{Write-Host (Split-Path -Path ($_.DirectoryName) -Leaf)+$_.Extension}
+
+# Rename folders longer than the configured maximum to prevent folder rename errors
+$names = Get-ChildItem -Path $Source -Recurse -Directory
+foreach($name in $names)
+{
+    if ($name.name.Length -gt $trimLength)
+    {
+        $newname = $name.name.Substring(0,$trimLength)
+        Rename-Item $name.FullName -NewName $newname -Verbose
+    }
+}
+
+# Rename files
+Get-ChildItem $Source -Filter $filter -Recurse | Rename-Item -NewName { (Split-Path -Path ($_.DirectoryName) -Leaf)+$_.Extension}
+
+# Move files - Make one line? Should be able to move the 2nd line up and drop the variable
+$files = Get-ChildItem ($Source + "\*")
+Get-ChildItem $files | Move-Item -Destination $Source
+
+# Remove all of the (now) empty folders
+& $tailRecursion -Path $Source
